0bc97e9d018e6a969b8ff74b76dae176b23f33d2
NaviBhukya <142711635+NaviBhukya@users.noreply.github.com>
2024-07-23 19:15:23 +0530

    Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Welcome to My Hobby Hub!</h2>
+      <p>Connect, share, and chat with like-minded individuals.</p>
+    </div>
+  );
+};
+
+export default Home;
